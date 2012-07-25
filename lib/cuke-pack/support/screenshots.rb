00324b053846fcf2dc774ba5cc139c166a209719
John Bintz <john@coswellproductions.com>
2012-07-25 13:57:43 -0400

    basic screenshot support

---
+++ lib/cuke-pack/support/screenshots.rb
@@ -0,0 +1,22 @@
+module CukePack
+  class << self
+    attr_accessor :screenshot_options
+  end
+
+  self.screenshot_options = { :width => 1280, :height => 1024, :prepend_driver_name => true, :directory => "features/screenshots" }
+end
+
+def take_screenshot(name, options = {})
+  options = CukePack.screenshot_options.merge(options)
+
+  selenium = Capybara.current_session.driver.browser
+  selenium.manage.window.resize_to(options[:width], options[:height])
+
+  target = options[:directory]
+  target = File.join(target, Capybara.current_driver.to_s)
+  target = File.join(target, name + ".png")
+
+  FileUtils.mkdir_p File.dirname(target)
+
+  selenium.save_screenshot(target)
+end
