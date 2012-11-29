afa43d71281b2c88937a2a0b4a1ce19e0949c5a5
John Bintz <john@coswellproductions.com>
2012-11-29 06:42:08 -0500

    turn off the directory opening thing, it's just annoying

--- lib/cuke-pack/support/step_writer.rb
+++ lib/cuke-pack/support/step_writer.rb
@@ -1,13 +1,2 @@
 require 'cucumber/step_writer'
 
-Cucumber::StepWriter.after_write do |dir|
-  %w{open xdg-open}.each do |cmd|
-    %x{which #{cmd}}
-
-    if $?.exitstatus
-      system %{#{cmd} #{dir}}
-      break
-    end
-  end
-end
-
